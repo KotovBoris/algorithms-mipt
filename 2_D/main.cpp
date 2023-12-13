6ad09355d6884fb6d42ee8fdebe93daa15c74726
KotovBoris <144000735+KotovBoris@users.noreply.github.com>
2023-12-14 01:19:09 +0300

    class, PrintOk

--- 2_D/main.cpp
+++ 2_D/main.cpp
@@ -1,4 +1,4 @@
-// Поддерживать минимум на очереди, запросов <= 2*10^5 
+// Поддерживать минимум на очереди, запросов <= 2*10^5
 
 #include <deque>
 #include <iostream>
@@ -6,59 +6,95 @@
 #include <string>
 #include <vector>
 
-void AddToMin(std::deque<size_t>& min, size_t iq) {
-  while (!min.empty() && min.back() > iq) {
-    min.pop_back();
+class QueueWithMin {
+ public:
+  void Push(size_t element);
+
+  void Clear();
+
+  size_t Size() { return data_.size(); };
+
+  size_t Front() { return data_.front(); }
+
+  size_t Min() { return min_[0]; }
+
+  size_t PopFront();
+
+ private:
+  std::queue<size_t> data_;
+  std::deque<size_t> min_;
+};
+
+void QueueWithMin::Push(size_t element) {
+  while (!min_.empty() && min_.back() > element) {
+    min_.pop_back();
   }
-  min.push_back(iq);
+  min_.push_back(element);
+
+  data_.push(element);
 }
 
-void UpdateMin(std::deque<size_t>& min, size_t popped) {
-  if (min.front() == popped) {
-    min.pop_front();
-  }
+void QueueWithMin::Clear() {
+  data_ = {};
+  min_.clear();
 }
 
-void PrintOk() { std::cout << "ok\n"; }
+size_t QueueWithMin::PopFront() {
+  size_t popped = Front();
+  if (min_.front() == popped) {
+    min_.pop_front();
+  }
+  data_.pop();
+
+  return popped;
+}
 
 int main() {
   size_t number;
   std::cin >> number;
 
-  std::queue<size_t> hat;
-  std::deque<size_t> min;
+  QueueWithMin hat;
+
   for (size_t i = 0; i < number; ++i) {
     std::string type;
     std::cin >> type;
+
     if (type == "enqueue") {
       size_t iq;
       std::cin >> iq;
-      AddToMin(min, iq);
-      hat.push(iq);
-      PrintOk();
+      hat.Push(iq);
+      std::cout << "ok\n";
+      continue;
     }
+
     if (type == "clear") {
-      hat = {};
-      min.clear();
-      PrintOk();
-    } else if (type == "size") {
-      std::cout << hat.size() << '\n';
-    } else {
-      if (hat.empty()) {
-        std::cout << "error\n";
-        continue;
-      }
-      if (type == "min") {
-        std::cout << min[0] << '\n';
-      }
-      if (type == "dequeue") {
-        std::cout << hat.front() << '\n';
-        UpdateMin(min, hat.front());
-        hat.pop();
-      }
-      if (type == "front") {
-        std::cout << hat.front() << '\n';
-      }
+      hat.Clear();
+      std::cout << "ok\n";
+      continue;
+    }
+
+    if (type == "size") {
+      std::cout << hat.Size() << '\n';
+      continue;
+    }
+
+    if (hat.Size() == 0) {
+      std::cout << "error\n";
+      continue;
+    }
+
+    if (type == "min") {
+      std::cout << hat.Min() << '\n';
+      continue;
+    }
+
+    if (type == "dequeue") {
+      std::cout << hat.PopFront() << '\n';
+      continue;
+    }
+
+    if (type == "front") {
+      std::cout << hat.Front() << '\n';
     }
   }
 }
