b4240808fee56008f6bf7e62e24e6c67b48e056d
KotovBoris <144000735+KotovBoris@users.noreply.github.com>
2023-12-14 02:32:21 +0300

    class

--- 2_F/main.cpp
+++ 2_F/main.cpp
@@ -5,42 +5,85 @@
 #include <string>
 #include <vector>
 
-void SiftDown(std::vector<long long>& heap, size_t index,
-              std::vector<size_t>& index_by_id,
-              std::vector<size_t>& id_by_index) {
-  while (index * 2 + 1 < heap.size()) {
-    size_t right_child = index * 2 + 1;
-    size_t left_child = index * 2 + 2;
-    long long next = -1;
+class Heap {
+ public:
+  Heap(size_t request_num) : index_by_id_(request_num) {}
 
-    if ((left_child >= heap.size() || heap[right_child] <= heap[left_child]) &&
-        heap[right_child] < heap[index]) {
-      next = right_child;
-    } else if (left_child < heap.size() && heap[left_child] < heap[index]) {
-      next = left_child;
+  void Insert(long long element, size_t id);
+
+  void ExtractMin();
+
+  void DecreaseKey(size_t id, size_t shift);
+
+  long long GetMin() { return data_[0]; }
+
+ private:
+  std::vector<size_t> index_by_id_;
+  std::vector<size_t> id_by_index_;
+  std::vector<long long> data_;
+
+  void SiftDown(size_t index) {
+    while (index * 2 + 1 < data_.size()) {
+      size_t right_child = index * 2 + 1;
+      size_t left_child = index * 2 + 2;
+      long long next = -1;
+      if ((left_child >= data_.size() ||
+           data_[right_child] <= data_[left_child]) &&
+          data_[right_child] < data_[index]) {
+        next = right_child;
+      } else if (left_child < data_.size() &&
+                 data_[left_child] < data_[index]) {
+        next = left_child;
+      }
+
+      if (next == -1) {
+        return;
+      }
+
+      Swap(next, index);
+      index = next;
     }
-    if (next == -1) {
-      return;
-    }
-    std::swap(heap[next], heap[index]);
-    std::swap(index_by_id[id_by_index[next]], index_by_id[id_by_index[index]]);
-    std::swap(id_by_index[next], id_by_index[index]);
-    index = next;
   }
+
+  void SiftUp(size_t index) {
+    size_t parent = (index - 1) / 2;
+    while (index > 0 && data_[parent] > data_[index]) {
+      Swap(parent, index);
+      index = parent;
+      parent = (index - 1) / 2;
+    }
+  }
+
+  void Swap(size_t index1, size_t index2) {
+    std::swap(data_[index1], data_[index2]);
+    std::swap(index_by_id_[id_by_index_[index1]],
+              index_by_id_[id_by_index_[index2]]);
+    std::swap(id_by_index_[index1], id_by_index_[index2]);
+  }
+};
+
+void Heap::Insert(long long element, size_t id) {
+  data_.push_back(element);
+  id_by_index_.push_back(id);
+  index_by_id_[id] = data_.size() - 1;
+  SiftUp(data_.size() - 1);
 }
 
-void SiftUp(std::vector<long long>& heap, size_t index,
-            std::vector<size_t>& index_by_id,
-            std::vector<size_t>& id_by_index) {
-  size_t parent = (index - 1) / 2;
-  while (index > 0 && heap[parent] > heap[index]) {
-    std::swap(heap[parent], heap[index]);
-    std::swap(index_by_id[id_by_index[parent]],
-              index_by_id[id_by_index[index]]);
-    std::swap(id_by_index[parent], id_by_index[index]);
-    index = parent;
-    parent = (index - 1) / 2;
+void Heap::ExtractMin() {
+  Swap(0, data_.size() - 1);
+  id_by_index_.resize(id_by_index_.size() - 1);
+  data_.resize(data_.size() - 1);
+
+  if (data_.empty()) {
+    return;
   }
+
+  SiftDown(0);
+}
+
+void Heap::DecreaseKey(size_t id, size_t shift) {
+  data_[index_by_id_[id]] -= shift;
+  SiftUp(index_by_id_[id]);
 }
 
 int main() {
@@ -50,9 +93,7 @@ int main() {
 
   size_t request_num;
   std::cin >> request_num;
-  std::vector<size_t> index_by_id(request_num);
-  std::vector<size_t> id_by_index;
-  std::vector<long long> heap;
+  Heap heap(request_num);
 
   for (size_t i = 0; i < request_num; ++i) {
     std::string type;
@@ -60,32 +101,21 @@ int main() {
     if (type == "insert") {
       long long new_element;
       std::cin >> new_element;
-      heap.push_back(new_element);
-      id_by_index.push_back(i);
-      index_by_id[i] = heap.size() - 1;
-      SiftUp(heap, heap.size() - 1, index_by_id, id_by_index);
+      heap.Insert(new_element, i);
+      continue;
     }
     if (type == "getMin") {
-      std::cout << heap[0] << '\n';
+      std::cout << heap.GetMin() << '\n';
+      continue;
     }
     if (type == "extractMin") {
-      index_by_id[id_by_index[heap.size() - 1]] = 0;
-      id_by_index[0] = id_by_index[heap.size() - 1];
-      id_by_index.resize(id_by_index.size() - 1);
-      heap[0] = heap[heap.size() - 1];
-      heap.resize(heap.size() - 1);
-      if (heap.empty()) {
-        continue;
-      }
-      SiftDown(heap, 0, index_by_id, id_by_index);
+      heap.ExtractMin();
     }
     if (type == "decreaseKey") {
       size_t id;
       size_t shift;
       std::cin >> id >> shift;
-      id -= 1;
-      heap[index_by_id[id]] -= shift;
-      SiftUp(heap, index_by_id[id], index_by_id, id_by_index);
+      heap.DecreaseKey(id - 1, shift);
     }
   }
 }
