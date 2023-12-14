966175756edd5be855ba4f5a0c753bfb1b910479
KotovBoris <144000735+KotovBoris@users.noreply.github.com>
2023-12-14 03:59:57 +0300

    Шаблоны и копипаста

--- 2_H/main.cpp
+++ 2_H/main.cpp
@@ -1,27 +1,21 @@
-// Реализовать СД,  основанную на куче, на которой поддерживаются минимум и максимум
-// Можно добавлять элементы, удалять минимум/максимум. Запросов <= 2*10^5
+// Реализовать СД, основанную на куче,
+// на которой поддерживаются минимум и максимум
+// Можно добавлять элементы, удалять минимум/максимум.
+// Запросов <= 2*10^5
 
+#include <functional>
 #include <iostream>
 #include <string>
 #include <vector>
 
-bool Less(long long first, long long second) { return (first < second); }
-
-bool More(long long first, long long second) { return (first > second); }
-
+template <typename Comparator>
 class Heap {
  public:
-  // компаратор - условие кучи: Less => на min
-  Heap(size_t request_num, bool (*comparator)(long long, long long))
-      : size_(0),
-        data_({}),
-        comparator_(comparator),
-        index_by_id_(std::vector<size_t>(request_num)),
-        id_by_index_({}) {}
+  // компаратор - условие кучи: std::less => на min
+  Heap(size_t request_num) : size_(0), index_by_id_(request_num) {}
   void Insert(long long number, size_t id);
   size_t Size() const { return size_; }
   long long GetRoot() { return data_[0]; }
-  void DeleteRoot();
   void Erase(size_t id);
   void Clear();
   size_t GetRootId() { return id_by_index_[0]; }
@@ -32,71 +26,62 @@ class Heap {
   void SiftDown(size_t index);
   size_t size_;
   std::vector<long long> data_;
-  bool (*comparator_)(long long, long long);
   std::vector<size_t> index_by_id_;
   std::vector<size_t> id_by_index_;
 };
 
-void Heap::Swap(size_t index1, size_t index2) {
+template <typename Comparator>
+void Heap<Comparator>::Swap(size_t index1, size_t index2) {
   std::swap(data_[index1], data_[index2]);
   std::swap(index_by_id_[id_by_index_[index1]],
             index_by_id_[id_by_index_[index2]]);
   std::swap(id_by_index_[index1], id_by_index_[index2]);
 }
 
-void Heap::SiftUp(size_t index) {
+template <typename Comparator>
+void Heap<Comparator>::SiftUp(size_t index) {
   size_t parent = (index - 1) / 2;
-  while (index > 0 && comparator_(data_[index], data_[parent])) {
-    this->Swap(parent, index);
+  while (index > 0 && Comparator()(data_[index], data_[parent])) {
+    Swap(parent, index);
     index = parent;
     parent = (index - 1) / 2;
   }
 }
 
-void Heap::SiftDown(size_t index) {
+template <typename Comparator>
+void Heap<Comparator>::SiftDown(size_t index) {
   while (index * 2 + 1 < size_) {
     size_t right_child = index * 2 + 1;
     size_t left_child = index * 2 + 2;
     long long next = -1;
 
     if ((left_child >= size_ ||
-         comparator_(data_[right_child], data_[left_child])) &&
-        comparator_(data_[right_child], data_[index])) {
+         Comparator()(data_[right_child], data_[left_child])) &&
+        Comparator()(data_[right_child], data_[index])) {
       next = right_child;
     } else if (left_child < size_ &&
-               comparator_(data_[left_child], data_[index])) {
+               Comparator()(data_[left_child], data_[index])) {
       next = left_child;
     }
     if (next == -1) {
       return;
     }
-    this->Swap(next, index);
+    Swap(next, index);
     index = next;
   }
 }
 
-void Heap::Insert(long long number, size_t id) {
+template <typename Comparator>
+void Heap<Comparator>::Insert(long long number, size_t id) {
   ++size_;
   data_.push_back(number);
   id_by_index_.push_back(id);
   index_by_id_[id] = size_ - 1;
-  this->SiftUp(size_ - 1);
+  SiftUp(size_ - 1);
 }
 
-void Heap::DeleteRoot() {
-  --size_;
-  index_by_id_[id_by_index_[size_]] = 0;
-  id_by_index_[0] = id_by_index_[size_];
-  id_by_index_.resize(size_);
-  data_[0] = data_[size_];
-  data_.resize(size_);
-  if (data_.empty()) {
-    return;
-  }
-  this->SiftDown(0);
-}
-
-void Heap::Erase(size_t id) {
+template <typename Comparator>
+void Heap<Comparator>::Erase(size_t id) {
   --size_;
   size_t index = index_by_id_[id];
   index_by_id_[id_by_index_[size_]] = index;
@@ -107,21 +92,23 @@ void Heap::Erase(size_t id) {
   if (data_.empty()) {
     return;
   }
-  this->SiftDown(index);
-  this->SiftUp(index);
+  if (index < data_.size()) {
+    SiftDown(index);
+    SiftUp(index);
+  }
 }
 
-void Heap::Clear() {
+template <typename Comparator>
+void Heap<Comparator>::Clear() {
   size_ = 0;
-  data_ = {};
-  id_by_index_ = {};
+  data_.clear();
+  id_by_index_.clear();
 }
 
 class TwoOrderedHeap {
  public:
   TwoOrderedHeap(size_t request_num)
-      : heap_min_(Heap(request_num, Less)),
-        heap_max_(Heap(request_num, More)) {}
+      : heap_min_(request_num), heap_max_(request_num) {}
   void Insert(long long number, size_t id);
   void DeleteMin();
   long long GetMin() { return heap_min_.GetRoot(); }
@@ -134,8 +121,8 @@ class TwoOrderedHeap {
   }
 
  private:
-  Heap heap_min_;
-  Heap heap_max_;
+  Heap<std::less<long long>> heap_min_;
+  Heap<std::greater<long long>> heap_max_;
 };
 
 void TwoOrderedHeap::Insert(long long number, size_t id) {
@@ -145,14 +132,14 @@ void TwoOrderedHeap::Insert(long long number, size_t id) {
 
 void TwoOrderedHeap::DeleteMin() {
   size_t id_min = heap_min_.GetRootId();
-  heap_min_.DeleteRoot();
+  heap_min_.Erase(id_min);
   heap_max_.Erase(id_min);
 }
 
 void TwoOrderedHeap::DeleteMax() {
   size_t id_max = heap_max_.GetRootId();
-  heap_max_.DeleteRoot();
   heap_min_.Erase(id_max);
+  heap_max_.Erase(id_max);
 }
 
 int main() {
