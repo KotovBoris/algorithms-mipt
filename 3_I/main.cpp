47fbe9928464fc4b4cc9914cbcfe452ca37e833d
KotovBoris <144000735+KotovBoris@users.noreply.github.com>
2023-12-17 21:54:55 +0300

    Вынесены методы, структура заместо std::pair

--- 3_I/main.cpp
+++ 3_I/main.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <iostream>
+#include <limits>
 #include <vector>
 
 class SparseTable {
@@ -11,59 +12,21 @@ class SparseTable {
   int SecondStatistic(size_t left, size_t right);
 
  private:
+  struct Statistics {
+    size_t first;
+    size_t second;
+  };
+
   std::vector<int> data_;
   std::vector<size_t> degree2_;
-  std::vector<std::vector<std::pair<int, int>>> segment_statistics_;
+  std::vector<std::vector<Statistics>> segment_statistics_;
 
-  void CalculateDegree2() {
-    degree2_[1] = 0;
-    for (size_t i = 2; i < degree2_.size(); ++i) {
-      degree2_[i] = degree2_[i - 1];
+  void CalculateDegree2();
 
-      if ((i & (i - 1)) == 0) {
-        degree2_[i]++;
-      }
-    }
-  }
+  Statistics MergeStatistics(size_t degree, size_t first_index,
+                             size_t second_index);
 
-  std::pair<int, int> MergeStatistics(size_t degree, size_t first_index,
-                                      size_t second_index) {
-    std::pair<int, int> first = segment_statistics_[degree][first_index];
-    std::pair<int, int> second = segment_statistics_[degree][second_index];
-
-    std::vector<std::pair<int, int>> statistics = {
-        {data_[first.first], first.first},
-        {data_[first.second], first.second},
-        {data_[second.first], second.first},
-        {data_[second.second], second.second}};
-
-    std::sort(statistics.begin(), statistics.end());
-    std::unique(statistics.begin(), statistics.end());
-
-    return std::make_pair(statistics[0].second, statistics[1].second);
-  }
-
-  void CalculateStatistics() {
-    const int kInfinity = 2000000000;
-    data_.push_back(kInfinity);
-
-    size_t length = data_.size() - 1;
-    segment_statistics_[0].resize(length);
-    for (size_t i = 0; i < length; ++i) {
-      segment_statistics_[0][i] = std::make_pair(i, length);
-    }
-
-    for (size_t degree = 1; degree <= degree2_[length]; ++degree) {
-      size_t segment_length = (1 << degree);
-      size_t current_size = length - segment_length + 1;
-      segment_statistics_[degree].resize(current_size);
-
-      for (size_t i = 0; i < current_size; ++i) {
-        segment_statistics_[degree][i] =
-            MergeStatistics(degree - 1, i, i + segment_length / 2);
-      }
-    }
-  }
+  void CalculateStatistics();
 };
 
 SparseTable::SparseTable(const std::vector<int>& data)
@@ -79,6 +42,56 @@ int SparseTable::SecondStatistic(size_t left, size_t right) {
   size_t second_start = right - segment_length + 1;
 
   return data_[MergeStatistics(degree, left, second_start).second];
+}
+
+void SparseTable::CalculateDegree2() {
+  degree2_[1] = 0;
+  for (size_t i = 2; i < degree2_.size(); ++i) {
+    degree2_[i] = degree2_[i - 1];
+
+    if ((i & (i - 1)) == 0) {
+      degree2_[i]++;
+    }
+  }
+}
+
+SparseTable::Statistics SparseTable::MergeStatistics(size_t degree,
+                                                     size_t first_index,
+                                                     size_t second_index) {
+  Statistics first = segment_statistics_[degree][first_index];
+  Statistics second = segment_statistics_[degree][second_index];
+
+  std::vector<std::pair<int, size_t>> statistics = {
+      {data_[first.first], first.first},
+      {data_[first.second], first.second},
+      {data_[second.first], second.first},
+      {data_[second.second], second.second}};
+
+  std::sort(statistics.begin(), statistics.end());
+  std::unique(statistics.begin(), statistics.end());
+
+  return Statistics{statistics[0].second, statistics[1].second};
+}
+
+void SparseTable::CalculateStatistics() {
+  data_.push_back(std::numeric_limits<int>::max());
+
+  size_t length = data_.size() - 1;
+  segment_statistics_[0].resize(length);
+  for (size_t i = 0; i < length; ++i) {
+    segment_statistics_[0][i] = Statistics{i, length};
+  }
+
+  for (size_t degree = 1; degree <= degree2_[length]; ++degree) {
+    size_t segment_length = (1 << degree);
+    size_t current_size = length - segment_length + 1;
+    segment_statistics_[degree].resize(current_size);
+
+    for (size_t i = 0; i < current_size; ++i) {
+      segment_statistics_[degree][i] =
+          MergeStatistics(degree - 1, i, i + segment_length / 2);
+    }
+  }
 }
 
 int main() {
@@ -101,6 +114,6 @@ int main() {
     size_t left;
     size_t right;
     std::cin >> left >> right;
-    std::cout << sparse_table.SecondStatistic(--left, --right) << '\n';
+    std::cout << sparse_table.SecondStatistic(left - 1, right - 1) << '\n';
   }
 }
